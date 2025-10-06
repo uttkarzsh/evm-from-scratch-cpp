e1837a6825bd89ad277b2c949cbe39896d5ca4f3
Uttkarsh Pawan <utkbest@gmail.com>
2025-10-06 22:51:22 +0530

    update: template

--- evm.cpp
+++ evm.cpp
@@ -3,12 +3,36 @@
 
 class EVM{
     std::vector<uint64_t> stack;
-     
+    size_t pc = 0; //program counter
+    bool running = false;
+    
     void push(uint64_t value){
         stack.push_back(value);
     }
 
-    
+    uint64_t pop(){
+        if(stack.empty()){
+            throw std::runtime_error("stack is empty");
+        }
+        uint64_t val = stack.back();
+        stack.pop_back();
+        return val;
+    }
+
+    void execute(std::vector<uint8_t> bytecode){
+        while(running && pc < bytecode.size()){
+            uint8_t opcode = bytecode[pc++];
+
+            switch(opcode){
+
+                
+                default:
+                    throw std::runtime_error("Unsupported opcode: " + std::to_string(opcode));
+            }
+        }
+    }
+
+
 };
 
 int main(){
